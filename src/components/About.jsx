caae27cd75032fc007f10f86f4dc837bafbef579
Dylan S <dillymon.ds@gmail.com>
2025-12-05 17:54:08 -0700

    About modal dialog now has max height with a scoll function if too large.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { CloseButton } from "./toast/CloseButton";
  *
  * A reusable modal component that displays an About/Credits section.
  * The modal is conditionally rendered based on the show prop and includes
- * a close button, overlay dimming, and proper accessibility attributes.
+ * a close button, scrolling, and overlay dimming.
  *
  * @param {Object} props
  * @param {boolean} props.show - Controls whether the modal is visible.
@@ -18,12 +18,12 @@ export const About = ({ show, onClose }) => {
 
   return (
     <div
-      className="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
+      className=" fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
       role="dialog"
       aria-modal="true"
       aria-labelledby="modalTitle"
     >
-      <div className="relative w-full max-w rounded-lg bg-white p-6 shadow-lg">
+      <div className="max-h-150 overflow-y-auto relative w-full max-w rounded-lg bg-white p-6 shadow-lg">
         <CloseButton show={show} onClose={onClose} />
 
         <h2
